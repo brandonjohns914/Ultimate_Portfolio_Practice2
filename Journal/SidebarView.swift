986130cbfe194f4e90fa36ff12b83bf8b6784352
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-02-05 12:55:07 -0800

    sidebarview

--- Journal/SidebarView.swift
+++ Journal/SidebarView.swift
@@ -21,12 +21,50 @@ struct SidebarView: View {
     ///Two filtering view options
     let smartFilters: [Filter] = [.all, .recent]
     
+    ///FetcheRequest loads all theTags from CoreData based on tag.name
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.name)]) var tags: FetchedResults<Tag>
+
+    /// Converts all Tags into Filter objects in an array.
+    var tagFilters: [Filter] {
+        tags.map { tag in
+            Filter(id: tag.id ?? UUID(), name: tag.name ?? "No Name", icon: "tag", tag: tag)
+        }
+    }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        List(selection: $dataController.selectedFilter) {
+            /// Listing out the Filters for the User decide .all or .recent
+            Section("Smart Filters") {
+                /// Sorts by tagFilters and the filter selected navigatates to a new view
+                ForEach(smartFilters) { filter in
+                    /// Takes the User to the filter picked
+                    NavigationLink(value: filter) {
+                        Label(filter.name, systemImage: filter.icon )
+                    }
+                }
+            }
+            ///Listing out Tags and filters changes the view based on if a tag was selected or not.
+            Section("Tags") {
+                ForEach(tagFilters) { filter in
+                    NavigationLink(value: filter){
+                        Label(filter.name, systemImage: filter.icon)
+                    }
+                }
+            }
+        }
+        .toolbar {
+            Button {
+                dataController.deleteAll()
+                dataController.createSampleData()
+            } label : {
+                Label("ADD SAMPLES", systemImage: "flame")
+            }
+        }
     }
 }
 
 #Preview {
     SidebarView()
+        .environmentObject(DataController.preview)
 }
